8cb186402b063b909f03bb9702235a17a1dbbcd4
jaRue1 <jaruejohnson@outlook.com>
2022-04-30 02:49:11 -0400

    flushed out readMe

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Helpful link
 // https://www.codecademy.com/article/getting-user-input-in-node-js
 
+console.log("Welcome to Node CLI Calculator App")
+console.log("")
+console.log("Please read the acceptance criteria to get started.")
+
 // Intro
 
 // Ask for user name
